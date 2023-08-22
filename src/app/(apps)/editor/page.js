77d55354ead572a49074704de500dff8d081453e
HOSSAM-ELDIN <HOS-ELDIN@users.noreply.github.com>
2023-08-22 17:18:34 +0300

    Update page.js

--- src/app/(apps)/editor/page.js
+++ src/app/(apps)/editor/page.js
@@ -1,13 +1,13 @@
 "use client";
 import { useStateContext } from "@/contexts/ContextProvider";
 import {
-	HtmlEditor,
-	Image,
-	Inject,
-	Link,
-	QuickToolbar,
-	RichTextEditorComponent,
-	Toolbar,
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
 } from "@syncfusion/ej2-react-richtexteditor";
 
 import { EditorData } from "../../../../public/data/dummy";
@@ -15,18 +15,18 @@ import { EditorData } from "../../../../public/data/dummy";
 import { Header } from "@/components";
 
 const EditorPage = () => {
-	const { activeMenu } = useStateContext();
-	return (
-		<div className={activeMenu ? " md:ml-72 " : " w-full flex-2"}>
-			<div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl dark:text-gray-200 dark:bg-secondary-dark-bg">
-				<Header category={"App"} title={"Editor"} />
-				<RichTextEditorComponent id="Editor">
-					<EditorData />
-					<Inject services={[HtmlEditor, Image, Link, QuickToolbar, Toolbar]} />
-				</RichTextEditorComponent>
-			</div>
-		</div>
-	);
+  const { activeMenu } = useStateContext();
+  return (
+    <div className={activeMenu ? " md:ml-72 " : " w-full flex-2"}>
+      <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl dark:text-gray-200 dark:bg-secondary-dark-bg">
+        <Header category={"App"} title={"Editor"} />
+        <RichTextEditorComponent id="Editor">
+          <EditorData />
+          <Inject services={[HtmlEditor, Image, Link, QuickToolbar, Toolbar]} />
+        </RichTextEditorComponent>
+      </div>
+    </div>
+  );
 };
 
 export default EditorPage;

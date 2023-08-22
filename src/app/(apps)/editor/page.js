d5d91916fe999fac04b9ce049804ff505510457f
HOSSAM-ELDIN <HOS-ELDIN@users.noreply.github.com>
2023-08-22 17:40:48 +0300

    Update page.js

--- src/app/(apps)/editor/page.js
+++ src/app/(apps)/editor/page.js
@@ -20,10 +20,10 @@ const EditorPage = () => {
     <div className={activeMenu ? " md:ml-72 " : " w-full flex-2"}>
       <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl dark:text-gray-200 dark:bg-secondary-dark-bg">
         <Header category={"App"} title={"Editor"} />
-        <RichTextEditorComponent>
-          {/* <EditorData /> */}
+        {/* <RichTextEditorComponent>
+          <EditorData />
           <Inject services={[HtmlEditor, Image, Link, QuickToolbar, Toolbar]} />
-        </RichTextEditorComponent>
+        </RichTextEditorComponent> */}
       </div>
     </div>
   );
